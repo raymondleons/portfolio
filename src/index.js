2a6365b436f89432eb5bd9a45e1741245d199f8f
Raymond leon Sembiring <raymondleons1997@gmail.com>
2019-01-13 19:33:08 +0700

    nothing

--- src/index.js
+++ src/index.js
@@ -3,19 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/index';
 import 'semantic-ui-css/semantic.min.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-dom'
 import * as serviceWorker from './serviceWorker';
+import Pdf from './App/Pdf'
+
 
 ReactDOM.render(
-
-    <Route>
+    <Router>
         <div>
-            <Route exact path='/' component={App} />
-            <Route exact path='/resume' component={Pdf} />
+            <Route exact path="/" component={App} />
+            <Route exact path="/resume" component={Pdf} />
         </div>
-    </Route>
-    
-    , document.getElementById('root'));
+    </Router>
+    , document.getElementById('root')
+)
 
 // tool berfungsi untuk mengarahkan tampilan index sih 'mungkin'
 serviceWorker.unregister();
+
+blbala
