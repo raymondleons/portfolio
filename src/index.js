972f3753c3179586d357569321435ea569563327
Raymond leon Sembiring <raymondleons1997@gmail.com>
2019-01-13 17:46:44 +0700

    nothing edit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App/index';
+import 'semantic-ui-css/semantic.min.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+    <Route>
+        <div>
+            <Route exact path='/' component={App} />
+            <Route exact path='/resume' component={Pdf} />
+        </div>
+    </Route>
+    
+    , document.getElementById('root'));
+
+// tool berfungsi untuk mengarahkan tampilan index sih 'mungkin'
 serviceWorker.unregister();
